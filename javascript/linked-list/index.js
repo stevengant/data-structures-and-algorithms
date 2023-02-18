51fbcee0ba56946539106085db112d67f8c263c1
“stevengant” <sgant39@gmail.com>
2023-02-18 01:03:10 +0000

    401challenge05 complete

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,6 +1,47 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  
+  insert(value) {
+    // const oldHead = this.head;
+    this.head = new Node(value, this.head);
+    // this.head.next = oldHead
+  }
+
+  includes(value) {
+    let current = this.head;
+
+    while(current) {
+      if(current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  
+  toString() {
+    let current = this.head;
+    let text = '';
+    // return '{ banana } -> { apple } -> NULL';
+
+    while(current) {
+      text += `{ ${current.value} } -> `;
+
+      current = current.next;
+    }
+    return text + 'NULL';
+  }
+}
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 
 }
 
