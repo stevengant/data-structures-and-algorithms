348165adc11d13cfa4e2598ffe4aa201158a979d
Steven Gant <79810854+stevengant@users.noreply.github.com>
2023-03-08 19:17:59 -0800

    401challenge18 (#33)
    Co-authored-by: “stevengant” <“sgant39@gmail.com”>

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -29,6 +29,52 @@ class BinaryTree {
     this.root = null;
   }
 
+  add(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+
+    let current = this.root;
+
+    while (current) {
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+ 
+  fizzBuzzTree(tree) {
+    let newTree = new BinaryTree();
+    let current = tree.root;
+    let traverse = (node) => {
+      if (node.value % 15 === 0) {
+        newTree.add('FizzBuzz');
+      } else if (node.value % 5 === 0) {
+        newTree.add('Buzz');
+      } else if (node.value % 3 === 0) {
+        newTree.add('Fizz');
+      } else {
+        newTree.add(node.value);
+      }
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return newTree;
+  }
+
   preOrder() {  // notice the pattern!!!!
     const results = [];
 
@@ -180,13 +226,13 @@ class BinarySearchTree
 
 }
 
-// let tree = new BinaryTree();
-// tree.root = new Node(10);
-// tree.root.left = new Node(5);
-// tree.root.right = new Node(15);
-// tree.root.left.left = new Node(1);
-// tree.root.left.right = new Node(8);
-// tree.root.right.right = new Node(17);
+let tree = new BinaryTree();
+tree.root = new Node(10);
+tree.root.left = new Node(5);
+tree.root.right = new Node(15);
+tree.root.left.left = new Node(1);
+tree.root.left.right = new Node(8);
+tree.root.right.right = new Node(17);
 
 // console.log(tree.getMax());
 
