8785535fbda795bdd76b0474dcadb300e9f8dc03
“stevengant” <sgant39@gmail.com>
2023-03-22 21:44:16 +0000

    401challenge28

--- javascript/sorting/comparisons/sort.js
+++ javascript/sorting/comparisons/sort.js
@@ -23,12 +23,12 @@ function compareTitle(a, b) {
 
   if(a.title.slice(0, 4) === 'The ') aTitle = a.title.slice('4');
   if(b.title.slice(0, 4) === 'The ') bTitle = b.title.slice('4');
-  
+
   // if(a.title.includes('The ')) a.title.replace('The ', '');
   // if(b.title.includes('The ')) b.title.replace('The ', '');
   // if(a.title === b.title) return 0;
-  // if (a.title < b.title) return 1;
-  // if (a.title > b.title) return -1;
+  // if(a.title < b.title) return 1;
+  // if(a.title > b.title) return -1;
 
   return aTitle > bTitle ? 1 : -1;
 }
