d99c3da991fb1d3c3d3e9bf6756505e10372e2d4
“stevengant” <sgant39@gmail.com>
2023-04-03 23:55:42 +0000

    401challenge36

--- javascript/graph/graph.test.js
+++ javascript/graph/graph.test.js
@@ -2,20 +2,75 @@
 
 const {Graph} = require('../graph');
 
-describe('Graph', () => {
-  it('Can add vertices', () => {
-    const graph = new Graph;
-    graph.addVertex('Apple');
-    expect(graph.adjacencyList.get('Apple')).toEqual([]);
+describe('breadthFirst', () => {
+  it('visits all vertices', () => {
+    const graph = new Graph();
+
+    const a = graph.addVertex('A');
+    const b = graph.addVertex('B');
+    const c = graph.addVertex('C');
+    const d = graph.addVertex('D');
+    graph.addEdge(a, b);
+    graph.addEdge(a, c);
+    graph.addEdge(b, d);
+    graph.addEdge(c, d);
+
+    const visited = [];
+    graph.breadthFirst(a, (value) => visited.push(value));
+
+    expect(visited).toEqual(['A', 'B', 'C', 'D']);
   });
 
-  it('Can add edges to vertices', () => {
-    const graph = new Graph;
-    graph.addVertex('Apple');
-    graph.addVertex('Strawberry');
-    graph.addDirectedEdge('Apple', 'Strawberry');
+  it('does not visit vertices multiple times', () => {
+    const graph = new Graph();
 
-    expect(graph.adjacencyList.get('Strawberry')[0].node).toEqual('Apple');
-    expect(graph.adjacencyList.get('Apple')[0].node).toEqual('Strawberry');
+    const a = graph.addVertex('A');
+    const b = graph.addVertex('B');
+    const c = graph.addVertex('C');
+    const d = graph.addVertex('D');
+    graph.addEdge(a, b);
+    graph.addEdge(a, c);
+    graph.addEdge(b, d);
+    graph.addEdge(c, d);
+
+    const visited = graph.breadthFirst(a);
+    expect(visited.size).toBe(4);
   });
-})
+
+  it('handles disconnected graphs', () => {
+const graph = new Graph();
+
+    const a = graph.addVertex('A');
+    const b = graph.addVertex('B');
+    const c = graph.addVertex('C');
+    const x = graph.addVertex('X');
+    const y = graph.addVertex('Y');
+    const z = graph.addVertex('Z');
+    graph.addEdge(a, b);
+    graph.addEdge(b, c);
+    graph.addEdge(x, y);
+    graph.addEdge(y, z);
+
+    const visited = graph.breadthFirst(a);
+    expect(visited.size).toBe(3);
+
+    const visited2 = graph.breadthFirst(x);
+    expect(visited2.size).toBe(3);
+  });
+
+  it('handles graphs with cycles', () => {
+    const graph = new Graph();
+
+    const a = graph.addVertex('A');
+    const b = graph.addVertex('B');
+    const c = graph.addVertex('C');
+    const d = graph.addVertex('D');
+    graph.addEdge(a, b);
+    graph.addEdge(b, c);
+    graph.addEdge(c, d);
+    graph.addEdge(d, b); // create cycle
+  
+    const visited = graph.breadthFirst(a);
+    expect(visited.size).toBe(4);
+  });
+});
--- javascript/graph/index.js
+++ javascript/graph/index.js
@@ -1,107 +1,109 @@
 'use strict';
 
 class Vertex {
-  constructor(value) {
-    this.value  = value;
+  constructor(value){
+    this.value = value;
   }
 }
 
 class Edge {
-  constructor(vertex, weight=0) {
+  constructor(vertex, weight=0){
     this.vertex = vertex;
     this.weight = weight;
   }
 }
 
 class Graph {
-  constructor() {
+  constructor(){
     this.adjacencyList = new Map();
   }
 
-  addVertex(value) {
+  addVertex(value){
     const vertex = new Vertex(value);
-    this.adjacencyList.set(vertex, []);
-    return vertex;
+    this.adjacencyList.set(vertex, [])
+    return vertex
   }
 
-  addDirectedEdge(startVertex, endVertex, weight=0) {
-    // find vertex to connect and grab it's edge arr
-    // no validation...yet
+  addEdge(startVertex, endVertex, weight=0){
+    // find the vertex we want to connect and grab it's edge array
+    // notice there is no validation... yet
     const neighbor = this.adjacencyList.get(startVertex);
-    neighbor.push(new Edge(endVertex, weight));
+    // console.log(neighbor)
+    neighbor.push( new Edge(endVertex, weight))
   }
 
-  getNeighbors(vertex) {
+  getNeighbors(vertex){
     return [...this.adjacencyList.get(vertex)];
   }
 
-  getNodes() {
+  // this is good enough!  let's call it!
+  getNodes(){
     let iterator = this.adjacencyList.keys();
-    // extract all the keys
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/keys
+    // return all the keys 
     return iterator;
   }
 
-  breadthFirst(root, callback) {
+  breadthFirst(root, callback){
     const queue = [root];
     const visited = new Set();
     visited.add(root);
     let current = null;
 
-    while(queue.length) {
-      // the first time we pop the vertex
+    while(queue.length){
+      // the first time, we pop the vertex
       current = queue.pop();
 
-      // if callback exists, we use it to do the thing
-      if(callback) {
+      // if a callback exists, we use it to "do the thing"
+      if (callback) {
         callback(current.value)
       }
 
-      // grab neighbor if it exists
+      // grab neighbor if it exists 
       const neighbors = this.getNeighbors(current);
-      for(let edge of neighbors) {
-        if(!visited.has(edge.vertex)) {
+      for (let edge of neighbors){
+        if (!visited.has(edge.vertex)){
           visited.add(edge.vertex);
-          queue.unshift(edge.vertex);
+          queue.unshift(edge.vertex)
         }
       }
     }
-
     return visited;
   }
 
-  depthFirst(root, callback) {
+  depthFirst(root, callback){
     const stack = [root];
     const visited = new Set();
     visited.add(root);
     let current = null;
 
-    while(stack.length) {
-      // the first time we pop the vertex
+    while(stack.length){
+      // the first time, we pop the vertex
       current = stack.pop();
 
-      // if callback exists, we use it to do the thing
-      if(callback) {
+      // if a callback exists, we use it to "do the thing"
+      if (callback) {
         callback(current.value)
       }
 
-      // grab neighbor if it exists
+      // grab neighbor if it exists 
       const neighbors = this.getNeighbors(current);
-      for(let edge of neighbors) {
-        if(!visited.has(edge.vertex)) {
+      for (let edge of neighbors){
+        if (!visited.has(edge.vertex)){
           visited.add(edge.vertex);
-          stack.push(edge.vertex);
+          stack.push(edge.vertex)
         }
       }
     }
-
     return visited;
   }
 
-  size() {
+  size(){
     return this.adjacencyList.size;
   }
 }
 
+module.exports = { Vertex, Graph, Edge };
 // const graph = new Graph();
 
 // const A = graph.addVertex('A');
@@ -124,9 +126,7 @@ class Graph {
 // graph.addDirectedEdge(F, E);
 
 // graph.breadthFirst(A, console.log);
-// console.log('=================');
-// graph.depthFirst(A, console.log);
-// console.log('=================');
+// console.log('-------------------');
+// graph.depthFirst(A, console.log)
+// console.log('-------------------');
 // console.log(graph.size());
-
-module.exports = { Graph };
