3a900c1c807058823e8d2d192e9270f1b3210d67
Steven Gant <79810854+stevengant@users.noreply.github.com>
2023-04-04 14:35:46 -0700

    401challenge37 (#47)
    Co-authored-by: “stevengant” <“sgant39@gmail.com”>

--- javascript/trees/trees.test.js
+++ javascript/trees/trees.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const { Node, BinaryTree, BinarySearchTree } = require('./index');
 
 describe('BinaryTree', () => {
@@ -52,6 +51,22 @@ describe('BinaryTree', () => {
 
     expect(max).toEqual(29);
   });
+
+  it('inverts the binary tree', () => {
+    let tree = new BinaryTree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(5);
+    tree.root.right = new Node(15);
+    tree.root.left.left = new Node(1);
+    tree.root.left.right = new Node(8);
+    tree.root.right.right = new Node(17);
+
+    const actualOutput = invert(tree);
+
+    expect(actualOutput).toEqual([10, 5, 15, 1, 8, 17]);
+  });
+
+
 
 });
 
